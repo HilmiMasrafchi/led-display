c98c661d926a28de28aad2e450707c2dd51ee0aa
Hilmi Masrafchi <jilt3d.uni@gmail.com>
2017-03-10 21:20:07 +0200

    removing MatrixIterationEventListener interface

--- led-display/led-display-api/src/main/java/me/hmasrafchi/leddisplay/api/Scene.java
+++ led-display/led-display-api/src/main/java/me/hmasrafchi/leddisplay/api/Scene.java
@@ -7,6 +7,10 @@ package me.hmasrafchi.leddisplay.api;
  * @author michelin
  *
  */
-public interface Scene extends MatrixIterationEventListener {
+public interface Scene {
+	Led onLedVisited(Led led, int ledRowIndex, int ledColumnIndex);
+
+	void onMatrixIterationEnded();
+
 	boolean isExhausted();
 }
