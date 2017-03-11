5e120b19bedf5255454a61278b63e5d4950e0f63
Hilmi Masrafchi <jilt3d.uni@gmail.com>
2017-03-11 16:26:31 +0200

    implementing replace overlays through REST api

--- led-display/led-display-api/src/main/java/me/hmasrafchi/leddisplay/api/Scene.java
+++ led-display/led-display-api/src/main/java/me/hmasrafchi/leddisplay/api/Scene.java
@@ -7,6 +7,8 @@ package me.hmasrafchi.leddisplay.api;
  * @author michelin
  *
  */
+// TODO: think about how I can hide this interface, maybe using Builder to build
+// Matrix using addScenes methods
 public interface Scene {
 	Led onLedVisited(Led led, int ledRowIndex, int ledColumnIndex);
 
--- led-display/led-display-webservices-rest/src/main/java/me/hmasrafchi/leddisplay/rest/SceneResource.java
+++ led-display/led-display-webservices-rest/src/main/java/me/hmasrafchi/leddisplay/rest/SceneResource.java
@@ -12,7 +12,11 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import me.hmasrafchi.leddisplay.rest.persist.MatrixRepository;
 import me.hmasrafchi.leddisplay.rest.persist.Overlay;
@@ -33,38 +37,106 @@ public class SceneResource {
 	}
 
 	@POST
+	@Path("stationary")
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("{sceneIndex}/overlays/stationary")
-	public void addOverlayStationary(@PathParam("matrixId") final int matrixId,
-			@PathParam("sceneIndex") final int sceneIndex, final OverlayStationary addOverlayStationary) {
-		add(matrixId, sceneIndex, addOverlayStationary);
+	public Response appendOverlayStationary(@PathParam("matrixId") final int matrixId,
+			final OverlayStationary addOverlayStationary, @Context UriInfo uriInfo) {
+		return appendOverlay(matrixId, addOverlayStationary, uriInfo);
 	}
 
-	@PUT
+	@POST
+	@Path("roll")
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("{sceneIndex}/overlays/{overlayIndex}/stationary")
-	public void addOverlayStationaryAt(@PathParam("matrixId") final int matrixId,
-			@PathParam("sceneIndex") final int sceneIndex, @PathParam("overlayIndex") final int overlayIndex,
-			final OverlayStationary addOverlayStationary) {
+	public Response appendOverlayRollHorizontally(@PathParam("matrixId") final int matrixId,
+			final OverlayRollHorizontally addRollHorizontally, @Context UriInfo uriInfo) {
+		return appendOverlay(matrixId, addRollHorizontally, uriInfo);
+	}
+
+	private Response appendOverlay(final int matrixId, final Overlay overlay, final UriInfo uriInfo) {
+		return matrixRepository.get(matrixId).map(matrixEntity -> {
+			final List<Scene> scenes = matrixEntity.getScenes();
+			final Scene scene = new Scene();
+			scene.getOverlays().add(overlay);
+			scenes.add(scene);
+			matrixRepository.update(matrixEntity);
+
+			final UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			// TODO: put the scene id
+			return Response.created(builder.build()).build();
+		}).orElse(Response.status(Response.Status.NOT_FOUND).build());
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{sceneIndex}/overlays/stationary")
+	public Response appendOverlayStationaryToScene(@PathParam("matrixId") final int matrixId,
+			@PathParam("sceneIndex") final int sceneIndex, final OverlayStationary addOverlayStationary,
+			@Context UriInfo uriInfo) {
+		return appendOverlayToScene(matrixId, sceneIndex, addOverlayStationary, uriInfo);
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("{sceneIndex}/overlays/roll")
-	public void addOverlayRollHorizontally(@PathParam("matrixId") final int matrixId,
-			@PathParam("sceneIndex") final int sceneIndex, final OverlayRollHorizontally addOverlayRollHorizontally) {
-		add(matrixId, sceneIndex, addOverlayRollHorizontally);
+	public Response appendOverlayRollHorizontallyToScene(@PathParam("matrixId") final int matrixId,
+			@PathParam("sceneIndex") final int sceneIndex, final OverlayRollHorizontally addOverlayRollHorizontally,
+			@Context UriInfo uriInfo) {
+		return appendOverlayToScene(matrixId, sceneIndex, addOverlayRollHorizontally, uriInfo);
 	}
 
-	private void add(Integer matrixId, Integer sceneIndex, Overlay overlay) {
-		matrixRepository.get(matrixId).ifPresent(matrixEntity -> {
+	private Response appendOverlayToScene(final int matrixId, final int sceneIndex, final Overlay overlay,
+			final UriInfo uriInfo) {
+		return matrixRepository.get(matrixId).map(matrixEntity -> {
 			final List<Scene> scenes = matrixEntity.getScenes();
 			if (sceneIndex == scenes.size()) {
-				scenes.add(new Scene());
+				return Response.status(Response.Status.NOT_FOUND).build();
 			}
 
 			scenes.get(sceneIndex).getOverlays().add(overlay);
 			matrixRepository.update(matrixEntity);
-		});
+
+			final UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			return Response.created(builder.build()).build();
+		}).orElse(Response.status(Response.Status.NOT_FOUND).build());
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{sceneIndex}/overlays/{overlayIndex}/stationary")
+	public Response replaceWithOverlayStationaryAtScene(@PathParam("matrixId") final int matrixId,
+			@PathParam("sceneIndex") final int sceneIndex, @PathParam("overlayIndex") final int overlayIndex,
+			final OverlayStationary addOverlayStationary, @Context final UriInfo uriInfo) {
+		return replaceWithOverlayAtScene(matrixId, sceneIndex, overlayIndex, addOverlayStationary, uriInfo);
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("{sceneIndex}/overlays/{overlayIndex}/roll")
+	public Response replaceWithOverlayRollHorizontallyAtScene(@PathParam("matrixId") final int matrixId,
+			@PathParam("sceneIndex") final int sceneIndex, @PathParam("overlayIndex") final int overlayIndex,
+			final OverlayRollHorizontally addOverlayRollHorizontally, @Context final UriInfo uriInfo) {
+		return replaceWithOverlayAtScene(matrixId, sceneIndex, overlayIndex, addOverlayRollHorizontally, uriInfo);
+	}
+
+	public Response replaceWithOverlayAtScene(final int matrixId, final int sceneIndex, final int overlayIndex,
+			final Overlay overlay, final UriInfo uriInfo) {
+		return matrixRepository.get(matrixId).map(matrixEntity -> {
+			final List<Scene> scenes = matrixEntity.getScenes();
+			if (sceneIndex == scenes.size()) {
+				return Response.status(Response.Status.NOT_FOUND).build();
+			}
+
+			final Scene scene = scenes.get(sceneIndex);
+			final List<Overlay> overlays = scene.getOverlays();
+			if (overlayIndex == overlays.size()) {
+				return Response.status(Response.Status.NOT_FOUND).build();
+			}
+
+			overlays.set(overlayIndex, overlay);
+			matrixRepository.update(matrixEntity);
+
+			final UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+			return Response.created(builder.build()).build();
+		}).orElse(Response.status(Response.Status.NOT_FOUND).build());
 	}
 }
